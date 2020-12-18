339fdce32321dc1db5bb92a5338184bce326acfd
keshav-kumar1 <74642647+keshav-kumar1@users.noreply.github.com>
2020-12-18 12:00:59 +0530

    Add files via upload

---
+++ Ground.js
@@ -0,0 +1,47 @@
+class Ground{
+constructor(x,y,width,heigth){
+
+var options={
+
+ isStatic:true
+
+
+}
+
+
+
+
+
+
+this.body=Bodies.rectangle(x,y,width,heigth,options)
+this.width=width
+this.heigth=heigth
+World.add(world,this.body)
+
+
+
+}
+
+display(){
+var pos=this.body.position
+rectMode(CENTER)
+fill("purple")
+rect(pos.x,pos.y,this.width,this.heigth)
+
+
+
+
+
+}
+
+
+
+}
+
+
+
+
+
+
+
+
